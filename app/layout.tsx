a0064a3d259e4887df844bec70dce7baa3d1c5c7
muchiyama2 <masatomo.uchiyama@jbs.com>
2024-11-19 12:54:10 +0900

    add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <GoogleTagManager gtmId="GTM-W5B26FP9" />
         <GoogleAnalytics gaId="G-7BVJTTS6YT"></GoogleAnalytics>
         {children}
       </body>
